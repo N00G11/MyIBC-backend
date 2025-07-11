28d5b4ea1162c3ed8e937837a5b40e11ee1fe94c
N00G11 <gnsanan@gmail.com>
2025-07-11 13:48:17 +0100

    new test

--- src/main/java/com/app/MyIBC/Authentification/config/SecurityConfig.java
+++ src/main/java/com/app/MyIBC/Authentification/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                             .anyRequest().authenticated();
                 })
                 .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("http://localhost:3000/auth/callback", true)
+                        .defaultSuccessUrl("/api/auth/success", true)
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(oauth2UserService())
                         )
